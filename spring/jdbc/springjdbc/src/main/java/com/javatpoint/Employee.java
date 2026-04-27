addfab9baa06a98f3077b0e87edf7ecb5e7c2d82
Yashaswini Mudragadda <mudragaddarajaniyashaswini@gmail.com>
2026-04-27 17:36:44 +0530

    Add files via upload

---
+++ spring/jdbc/springjdbc/src/main/java/com/javatpoint/Employee.java
@@ -0,0 +1,60 @@
+package com.javatpoint;
+public class Employee 
+
+{  
+
+private int id;  
+
+private String name;  
+
+private float salary;  
+
+//no-arg and parameterized constructors  
+
+//getters and setters  
+
+public int getId() {
+return id;
+
+}
+
+public void setId(int id) {
+
+this.id = id;
+
+}
+
+public String getName() {
+
+return name;
+
+}
+
+public void setName(String name) {
+this.name = name;
+
+}
+
+public float getSalary() {
+
+return salary;
+
+}
+
+public void setSalary(float salary) {
+	this.salary = salary;
+	}
+
+public Employee(int id, String name, float salary) {
+
+super();
+
+this.id = id;
+
+this.name = name;
+
+this.salary = salary;
+
+}
+
+}  
